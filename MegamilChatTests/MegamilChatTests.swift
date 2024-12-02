711affb56d0336257e176e6059be1956c5b77f85
Eduardo dos santos <eduardo@megamil.com.br>
2024-12-02 16:00:02 -0300

    ✨ add tests

---
+++ MegamilChatTests/MegamilChatTests.swift
@@ -0,0 +1,14 @@
+//
+//  MegamilChatTests.swift
+//  MegamilChatTests
+//
+//  Created by Eduardo dos santos on 02/12/24.
+//
+
+import Testing
+
+struct MegamilChatTests {
+
+    @Test func example() async throws { }
+
+}
